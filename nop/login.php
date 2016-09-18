d941ad43b620b74d4b35ba927f7a49f784b8672f
noppanutn <noppanutn@gmail.com>
2016-09-18 16:33:06 +0700

    change

--- nop/login.php
+++ nop/login.php
@@ -2,42 +2,9 @@
 require ('function.php');
 
 if(isset($_POST['username']) and isset($_POST['password'])){
-<<<<<<< HEAD
   $_SESSION['user']=$_POST['username'];
   $_SESSION['pass']=$_POST['password'];
   check_login($_SESSION['user'],$_SESSION['pass']);
-=======
-  /*echo $_POST['username'];
-  echo $_POST['password'];*/
-
-  /* $line=1;*/
-  $myfile = fopen("source.txt","r");
-  $user=$_POST['username'];
-  $pass=$_POST['password'];
-  while(!feof($myfile)){
-    $break_array = explode(',',fgets($myfile)); /* break a line of text in txt file by ',' into an aray */
-  /*  echo $break_array[0]." ";
-    echo $break_array[1]." "; */
-    $check_user = explode('=',$break_array[0]); /* get username */
-  /*  echo $check_user[1]." ";*/
-    $check_pass = explode('=',$break_array[1]); /* get password */
-  /*  echo $check_pass[1]." ";
-    echo $line;
-    $line++; */
-    if($check_user[1]==$user){
-      if($pass==$check_pass[1]){
-        echo "login successful";
-        header ('Location: main.html');
-      } else {
-        echo "wrong password";
-      }
-      break;
-    }
-  }
-  if($check_user[1]!=$user){
-    echo "user not found";
-  }
->>>>>>> refs/remotes/origin/master
 }
 ?>
 
