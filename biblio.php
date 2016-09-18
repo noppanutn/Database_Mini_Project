dc3d677bf035318d704316e0ca577257d454c1ca
hihihahahuhu <firstcardistry@gmail.com>
2016-09-18 20:05:55 +0700

    Add files via upload

---
+++ biblio.php
@@ -0,0 +1,106 @@
+<html>
+  <head>
+    <title>SIIT Registration Login Page</title>
+    <link rel="stylesheet"  href="main.css">
+	
+	<style type="text/css">
+	.tables { background: #FFFFF; color:#000CB5; width:600px;}
+	.tabletop { background: #9E8ABE; color:#000000;}
+	.t1 { background: #E5E0FF; width:200px; height:25px; color:#000000;}
+	.t2 { background: #F0F1FF;}
+</style>
+	
+	
+	
+  </head>
+  <body>
+    <div id="wrapper">
+
+      <div id="div_main">
+        <h1> &nbsp; MAIN <br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; MENU</h1>
+        <div id="div_menu">
+          <ul id="menu">
+            <li><a href="logout.html">LOG OUT</a></li>
+            <li><a href="change_password.html">Change Password</a></li>
+            <li><a href="timetable.php">Study Timtable</a></li>
+            <li><a href="biblio.php">Student Biblio</a></li>
+            <li><a href="grade.php">Grade Result</a></li>
+            <li><a href="faq.html">FAQ</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div id="div_content">
+
+        <div id="div_banner">
+          <img src="banner.jpg">
+        </div>
+        <div id="div_news">
+			<br>
+			<table border='0' Class='tables'>
+			<tr><th colspan="2"  class='tabletop'>Education Data</th></tr>
+			<tr><td class='t1'>STUDENT ID:</td>		<td class='t2'>5722770575</td><tr>
+			<tr><td class='t1'>PERSONAL ID:</td>	<td class='t2'>12345678910</td><tr>
+			<tr><td class='t1'>NAME(ENG):</td>		<td class='t2'>Name Surename</td><tr>
+			<tr><td class='t1'>NAME:</td>			<td class='t2'>name surename</td><tr>
+			<tr><td class='t1'>SCHOOL:</td>			<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>CAMPUS:</td>			<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>PROGRAM:</td>		<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>MINOR:</td>			<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>LEVEL:</td>			<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>DEGREE NAME:</td>	<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>ADMISSION YEAR:</td>	<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>STATUS:</td>			<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>ADMISSION TYPE:</td>	<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>PRE CERTIFICATE:</td><td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>GRADUATED FROM:</td>	<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>ADVISOR:</td>		<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>aaa</td>				<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>aaa</td>				<td class='t2'>bbb</td><tr>
+			</table>
+
+
+
+			<table border='1' Class='tables'>
+			<tr><th colspan="2"  class='tabletop'>Academic Status</th></tr>
+			<tr><td class='t1'>CREDIT ATTEMPT	:</td>		<td class='t2'>	82 </td><tr>
+			<tr><td class='t1'>CREDIT SATISFY</td>			<td class='t2'>	82 </td><tr>
+			<tr><td class='t1'>GPAX</td>					<td class='t2'> 4.00 </td><tr>
+			</table>
+
+
+
+			<table border='1' Class='tables'>
+			<tr><th colspan="2"  class='tabletop'>Education Data</th></tr>
+			<tr><td class='t1'>BIRTHDAY:</td>				<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>BIRTH PLACE:</td>			<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>NATIONALITY:</td>			<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>RELIGION:</td>				<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>BLOOD GROUP:</td>			<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>ADDRESS:</td>				<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>FATHER NAME:</td>			<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>MOTHER NAME:</td>			<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>GUARDIAN:</td>				<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>RELATION:</td>				<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>ADDRESS(GUARDIAN):</td>		<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>COMTACT PERSON:</td>			<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>NAME:</td>					<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>ADDRESS:</td>				<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>WORK STATUS:</td>			<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>COMPANY:</td>				<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>POSITION:</td>				<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>ADDRESS:</td>				<td class='t2'>bbb</td><tr>
+			</table>
+
+			<table border='1' Class='tables'>
+			<tr><th colspan="2"  class='tabletop'>Admission Information</th></tr>
+			<tr><td class='t1'>Admission Group</td>			<td class='t2'>1109:Computer Engineering</td><tr>
+			<tr><td class='t1'>Group</td>					<td class='t2'>bbb</td><tr>
+			<tr><td class='t1'>Admission Condition</td>		<td class='t2'>	FIX</td><tr>
+			</table>
+        </div>
+      </div>
+
+    </div>
+  </body>
+</html>
